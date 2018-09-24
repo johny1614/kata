77352dc2554b79a377e6618505581164b030bd03
johny1614@gmail.com <johny1614@gmail.com>
2018-09-24 21:14:50 +0200

    do pusha

--- src/app/animacje/angular-animations/rozwiazanie/rozwiazanie.component.ts
+++ src/app/animacje/angular-animations/rozwiazanie/rozwiazanie.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { trigger, state, style, transition, animate, group, keyframes, query } from '@angular/animations';
 
 @Component({
   selector: 'app-rozwiazanie',
@@ -40,8 +40,8 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
       state('clicked', style({
         'background-color': 'yellow'
       })),
-      state('mouseDown',style({
-        'background-color':'black'
+      state('mouseDown', style({
+        'background-color': 'black'
       })),
       transition('default->mouseDown', animate('400ms')),
       transition('mouseDown->clicked', animate('400ms')),
@@ -53,18 +53,64 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
       })),
       state('clicked', style({
         'background-color': 'yellow',
-        'border':'5px solid blue'
+        'border': '5px solid blue'
       })),
-      transition('default->clicked',[style({'border':'5px solid blue'}),animate('2000ms')]),
+      transition('default->clicked', [style({ 'border': '5px solid blue' }), animate('2000ms')]),
     ]),
-    trigger('sixth',[
-      transition('* => *',[
-        animate(1500,style({'width':'600px'})),
-        animate(900,style({'width':'200px'})),
-        animate(1000,style({'width':'*'}))
+    trigger('sixth', [
+      transition('* => *', [
+        animate(1500, style({ 'width': '600px' })),
+        animate(900, style({ 'width': '200px' })),
+        animate(1000, style({ 'width': '*' }))
       ]
-    )
+      )
+    ]),
+    trigger('seventh', [
+      transition('default => clicked', [
+        group([
+          animate(2000, style({ 'width': '600px' })),
+          animate(700, style({ 'background-color': 'green' }))
+        ])
+      ]
+      )
+    ]),
+    trigger('eigth',
+      [
+        transition('default->clicked', animate('6000ms', keyframes([
+          style({ 'background-color': 'blue', 'offset': 0.1 }),
+          style({ 'background-color': 'pink', 'offset': 0.8 }),
+          style({ 'background-color': 'yellow', 'offset': 0.9 }),
+          style({ 'background-color': 'red', 'offset': 1 }),
+        ])))
+      ]),
+    trigger('nineth', [
+      transition('default->clicked', [
+        query('.first', [
+          style({ 'transform': 'translateY(-200px)' }),
+          animate('300ms', style({ 'transform': 'translateY(0px)' }))
+        ]),
+        query('.second', [
+          style({ 'transform': 'translateX(200px)' }),
+          animate('300ms', style({ 'transform': 'translateX(0px)' }))
+        ])
+      ])
+    ]),
+    trigger('tenth', [
+      transition('default->clicked', [
+        group([
+          query('.first', [
+            style({ 'transform': 'translateY(-200px)' }),
+            animate('300ms', style({ 'transform': 'translateY(0px)' }))
+          ]),
+          query('.second', [
+            style({ 'transform': 'translateX(200px)' }),
+            animate('300ms', style({ 'transform': 'translateX(0px)' }))
+          ])
+        ]
+        )
+      ])
     ])
+
   ]
 })
 export class AngularAnimationsRozwiazanieComponent implements OnInit {
@@ -72,9 +118,13 @@ export class AngularAnimationsRozwiazanieComponent implements OnInit {
   firstState = 'default';
   secondState = 'default';
   thirdState = 'default';
-  fourthState='default';
-  fifthState='default';
-  sixthState='default';
+  fourthState = 'default';
+  fifthState = 'default';
+  sixthState = 'default';
+  seventhState = 'default';
+  eigthState = 'default';
+  ninethState = 'default';
+  tenthState = 'default';
   constructor() { }
 
   ngOnInit() {
